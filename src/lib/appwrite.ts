7c040c46c9992e3e94805ed9ee65c29a6b49216f
hopago <132269386+hopago@users.noreply.github.com>
2024-11-22 22:58:51 +0900

    members get, delete

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 import { cookies } from "next/headers";
 
-import { Client, Account, Databases } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { AUTH_COOKIE } from "@/features/auth/constants/cookie";
 
 export async function createSessionClient() {
@@ -34,6 +34,9 @@ export async function createAdminClient() {
   return {
     get account() {
       return new Account(client);
+    },
+    get users() {
+      return new Users(client);
     },
   };
 }
